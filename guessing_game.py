4c1b2e4541b3082899cad3ae43c839ae2ce6a3fb
amy gallacher <amy.gallacher@codeclan.com>
2021-07-26 14:25:01 +0100

    adds game logic

--- guessing_game.py
+++ guessing_game.py
@@ -1,6 +1,26 @@
-#wecome the user to the game
+#welcome the user to the game
 print("********************************")
 print("*                              *")
 print("* WELCOME TO THE GUESSING GAME *")
 print("*                              *")
 print("********************************")
+
+#import random number generator 
+
+import random
+
+#establish correct number
+
+number = random.randint(1,100)
+print(number)
+
+#ask user for guess and store in a variable
+
+guess = int(input("Give me a number between 1 and 100 : "))
+
+#check if guess was correct and respond appropriately
+
+if guess == number: 
+    print("You got it! You're a genius.")
+else:
+    print("Nope. You lose.")
