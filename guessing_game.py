c5bbbc7b0ab9c361880b2245a4be06aee7278ab8
amy gallacher <amy.gallacher@codeclan.com>
2021-07-26 14:13:40 +0100

    initial commit

---
+++ guessing_game.py
@@ -0,0 +1,6 @@
+#wecome the user to the game
+print("********************************")
+print("*                              *")
+print("* WELCOME TO THE GUESSING GAME *")
+print("*                              *")
+print("********************************")
